fa946a9cec932968dc5d18fb6557c2ba21d98e67
Dilawar Singh <dilawars@ncbs.res.in>
2015-08-15 22:42:17 +0530

    Launch it on travis.

--- test_all_scripts.sh
+++ test_all_scripts.sh
@@ -1,24 +1,62 @@
 #!/bin/bash
-set -e
+source ./colors.sh
+
+# matplotlibrc file.
+MATPLOTRC=./_travis/matplotlibrc
+
+PWD=$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)
+MAINLESS=$PWD/MAINLESS
+BLACKLISTED=$PWD/BLACKLISTED
+GUI=$PWD/GUISCRIPTS
+INTERACTIVE=$PWD/INTERACTIVE
+TORUN=$PWD/TORUN
+
+for f in $MAINLESS $BLACKLISTED $GUI $INTERACTIVE $TORUN; do
+    # Remove already existing files
+    if [ -f $f ]; then
+        rm -f $f
+    fi
+done
+
 declare -i a
 PYC=`which python`
-RUNDIR=./_test_dir_moose-examples_
-echo "Creating $RUNDIR"
-mkdir -p $RUNDIR
-MATPLOTRC=_travis/matplotlibrc
-echo "copying examples out of source"
-cp -r moose-examples/* $RUNDIR/
-PYFILES=`find $RUNDIR -name "*.py"`
+
+function check_file
+{
+    filepath="$1"
+    if grep -q "input(" $filepath
+    then
+        coloredPrint WARN "File contains input() call. Interactive script. WONT
+        RUN"
+        echo $filepath >> $INTERACTIVE
+    elif grep -q "QtGui" $filepath
+    then
+        coloredPrint WARN "File seems to be a GUI. WONT RUN"
+        echo $filepath >> $GUI
+    elif grep -q "__main__\|main(" $filepath
+    then
+        coloredPrint "INFO" "Script with main() or __main__"
+        echo $filepath >> $TORUN
+    fi
+}
+
+PYFILES=`find . -name "*.py"`
 for pyf in $PYFILES; do
     let a=a+1
     dn=`dirname $pyf`
     fn=`basename $pyf`
     # copy matplotlibrc file to working directory
-    cp $MATPLOTRC $dn/
+    check_file $pyf
+done
+
+# Run those files which are stored in $TORUN script.
+for f in `cat $TORUN`; do
+    basedir=`dirname $f`
+    filename=`basename $f`
+    cp $MATPLOTRC $basedir
     (
-        cd $dn
-        echo "Executing $fn in directory $dn"
-        $PYC $fn
+        coloredPrint INFO "Running file $f"
+        cd $basdir 
+        $PYC $filename >> __run__.log || echo "$f" >> FAILED
     )
 done
-echo "Total $a scripts run"
