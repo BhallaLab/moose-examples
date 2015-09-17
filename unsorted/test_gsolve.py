eed2d218be9586b2ed063a0d0637597bf8e402b3
Dilawar Singh <dilawars@ncbs.res.in>
2015-09-17 14:28:32 +0530

    Test script which causes seg-faults.

--- unsorted/test_gsolve.py
+++ unsorted/test_gsolve.py
@@ -1,29 +1,34 @@
 import moose
 compt = moose.CubeMesh('/compt')
+compt.volume = 1e-20
 
 a = moose.Pool('/compt/a')
 b = moose.Pool('/compt/b')
 
-a.concInit, b.concInit = 0.1, 0.2
+a.nInit, b.nInit = 100, 10
 
 atab = moose.Table2('/tableA')
 btab = moose.Table2('/tableB')
-moose.connect(atab, 'requestOut', a, 'getConc')
-moose.connect(btab, 'requestOut', b, 'getConc')
+moose.connect(atab, 'requestOut', a, 'getN')
+moose.connect(btab, 'requestOut', b, 'getN')
 
 # Add a function which set conc of a
-#func = moose.Function('/compt/a/function')
-func = moose.Function('/func')
-func.expr = 'y0*sin(t)'
+
+## NOTE: This cause seg-fault.
+func = moose.Function('/compt/a/function')
+
+# This does not.
+#func = moose.Function('/func')
+
+func.expr = '100*(1 + sin(0.1*t))'
 func.mode = 1
-moose.connect(func, 'requestOut', b, 'getConc') 
-moose.connect(func, 'valueOut', a, 'setConc')
+moose.connect(func, 'valueOut', a, 'setN')
 
 reac = moose.Reac('/compt/reac')
 moose.connect(reac, 'sub', a, 'reac')
 moose.connect(reac, 'prd', b, 'reac')
-reac.Kf = 1
-reac.Kb = 0.1
+reac.Kf = 2
+reac.Kb = 1
 
 gsolve = moose.Gsolve('/compt/gsolve')
 stoich = moose.Stoich('/compt/stoich')
@@ -33,7 +38,7 @@ stoich.path = '/compt/##'
 
 print("Reinit")
 moose.reinit()
-moose.start(1)
+moose.start(100)
 print("Done simulation")
 
 # plots
