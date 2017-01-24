d120484e938a60ee603076b2e40ac6e99ed5a0da
HarshaRani <ranishashi@gmail.com>
2017-01-24 12:18:09 +0530

    small correction for input eval with is str

--- snippets/IntegrateFireZoo.py
+++ snippets/IntegrateFireZoo.py
@@ -120,8 +120,8 @@ def main():
     #choiceKey = 'LIF'
     #### No need, am inputting it from the user on the terminal
     choiceKeys = list(neuronChoices.keys()) # keys() does not retain the order in dict defn above!
-    choiceIndex = eval(input('Choose a number corresponding to your desired neuron: '+ \
-                        str([(i,key) for (i,key) in enumerate(choiceKeys)])+' -- '))
+    choiceIndex = eval(str(input('Choose a number corresponding to your desired neuron: '+ \
+                         str([(i,key) for (i,key) in enumerate(choiceKeys)])+' -- ')))
     choiceKey = choiceKeys[choiceIndex]
     neuronChoice = neuronChoices[choiceKey]
 
