6aff3349c360ca8010fc9db047e9360e51798b59
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-24 17:29:52 +0530

    Fixed command to download gpg-key in script.

--- .travis_prepare.sh
+++ .travis_prepare.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt-get install cmake --force-yes
 sudo apt-get -y --force-yes install python-qt4 python-pip graphviz
 sudo apt-get -y --force-yes install python-h5py python-scipy python-pygraphviz
-wget -nv https://download.opensuse.org/repositories/home:moose/xUbuntu_$VERSION/Release.key -O Re    lease.key
+wget -nv https://download.opensuse.org/repositories/home:moose/xUbuntu_$VERSION/Release.key -O Release.key
 sudo apt-key add - < Release.key
 cat <<EOF > /etc/apt/sources.list.d/home:moose.list 
 deb http://download.opensuse.org/repositories/home:/moose/xUbuntu_${VERSION}/ /
