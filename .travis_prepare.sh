bf66addf1e2ea593c7b6e109331835978bfd92ea
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-24 17:42:39 +0530

    - run ./travis_prepare.sh script with sudo . - cleanup in readme.

--- .travis_prepare.sh
+++ .travis_prepare.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+# ROOT should run this script.
 VERSION=$(lsb_release -r | cut -f2)
-sudo apt-get -y update
-sudo apt-get install cmake --force-yes
-sudo apt-get -y --force-yes install python-qt4 python-pip graphviz
-sudo apt-get -y --force-yes install python-h5py python-scipy python-pygraphviz
+apt-get -y update
+apt-get install cmake --force-yes
+apt-get -y --force-yes install python-qt4 python-pip graphviz
+apt-get -y --force-yes install python-h5py python-scipy python-pygraphviz
 wget -nv https://download.opensuse.org/repositories/home:moose/xUbuntu_$VERSION/Release.key -O Release.key
-sudo apt-key add - < Release.key
+apt-key add - < Release.key
 cat <<EOF > /etc/apt/sources.list.d/home:moose.list 
 deb http://download.opensuse.org/repositories/home:/moose/xUbuntu_${VERSION}/ /
 EOF
-sudo apt-get update
-sudo apt-get install pymoose
+apt-get update
+apt-get install pymoose
