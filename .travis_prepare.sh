a287d9a453f164abc8426709d3a06870c63394d2
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-24 17:07:03 +0530

    Use OBS package. Breakdown of Travis flow into multiple script.

--- .travis_prepare.sh
+++ .travis_prepare.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+VERSION=$(lsb_release -r | cut -f2)
 sudo apt-get -y update
 sudo apt-get install cmake --force-yes
 sudo apt-get -y --force-yes install python-qt4 python-pip graphviz
 sudo apt-get -y --force-yes install python-h5py python-scipy python-pygraphviz
-sudo pip install python-libsbml networkx matplotlib numpy --upgrade
-sudo pip install pymoose numpy 
+wget -nv https://download.opensuse.org/repositories/home:moose/xUbuntu_$VERSION/Release.key -O Re    lease.key
+sudo apt-key add - < Release.key
+cat <<EOF > /etc/apt/sources.list.d/home:moose.list 
+deb http://download.opensuse.org/repositories/home:/moose/xUbuntu_${VERSION}/ /
+EOF
+sudo apt-get update
+sudo apt-get install pymoose
