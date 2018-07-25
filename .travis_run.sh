cc363bb58fd58e81e8c069d515e32aca5ce500cf
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-25 10:01:29 +0530

    use cat if file exists.

--- .travis_run.sh
+++ .travis_run.sh
@@ -4,11 +4,19 @@ mkdir -p _temp
     cd _temp && cmake .. && make -j4 
     echo "We did not run following scripts"
 
-    echo "Following scripts ran successfully"
-    cat ./SUCCEEDED
-    echo "Following scripts failed"
-    cat ./FAILED 
-    echo "Following scripts timed-out"
-    cat ./TIMEOUT
+    if [[ -f SUCCEEDED ]]; then
+        echo "Following scripts ran successfully"
+        cat ./SUCCEEDED
+    fi
+
+    if [ -f FAILED ]; then
+        echo "Following scripts failed"
+        cat ./FAILED 
+    fi
+
+    if [ -f TIMEOUT ]; then
+        echo "Following scripts timed-out"
+        cat ./TIMEOUT
+    fi
 
 )
