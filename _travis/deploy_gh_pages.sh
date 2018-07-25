65e2b09f73facd4fe3b10f1cd9b2933a67c2c3b1
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-25 14:04:10 +0530

    More details file.

--- _travis/deploy_gh_pages.sh
+++ _travis/deploy_gh_pages.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
 INDEXMD='index.md'
 FAILEDTXT=$(cat FAILED)
+BROKENTXT=""
+if [[ -f BROKEN ]]; then
+    BROKENTXT=$(cat BROKEN)
+fi
+
+BLACKLISTEDTXT=''
+if [[ -f BLACKLISTED ]]; then
+    #statements
+    BLACKLISTEDTXT=$(cat BLACKLISTED)
+fi
+
 cat <<-EOF > $INDEXMD 
-Following scripts failed.
+# Failed scripts
+
+These scripts failed to execute successfully on Travis.
+
 $FAILEDTXT
+
+# Broken scripts
+
+These scripts are marked `BROKEN` by one or more developers.
+
+$BROKENTXT
+
+# Blacklist script
+
+This scripts were not tested because they require human interaction.
+
+$BLACKLISTEDTXT
+
+
 EOF
