85bee3c8978d2170b9f759fdf558cab01ec926a1
Dilawar Singh <dilawars@ncbs.res.in>
2018-07-25 13:57:16 +0530

    run test in topmost level of directory.

--- .travis_run.sh
+++ .travis_run.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
-mkdir -p _temp
-( 
-    cd _temp && cmake -DPYTHON_EXECUTABLE=/usr/bin/python .. && make -j4 
-    echo "We did not run following scripts"
 
-    if [[ -f SUCCEEDED ]]; then
-        echo "Following scripts ran successfully"
-        cat ./SUCCEEDED
-    fi
+# Run in the current directory. Unfortunately travis is not pushing from other
+# directory.
+cmake -DPYTHON_EXECUTABLE=/usr/bin/python . && make -j4 
+echo "We did not run following scripts"
 
-    if [ -f FAILED ]; then
-        echo "Following scripts failed"
-        cat ./FAILED 
-    fi
+if [[ -f SUCCEEDED ]]; then
+    echo "Following scripts ran successfully"
+    cat ./SUCCEEDED
+fi
 
-    if [ -f TIMEOUT ]; then
-        echo "Following scripts timed-out"
-        cat ./TIMEOUT
-    fi
+if [ -f FAILED ]; then
+    echo "Following scripts failed"
+    cat ./FAILED 
+fi
 
-)
+if [ -f TIMEOUT ]; then
+    echo "Following scripts timed-out"
+    cat ./TIMEOUT
+fi
