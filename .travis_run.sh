e0bccce7a71b3c5d2d5fe8d231b732d63b098c81
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-14 16:20:49 +0530

    Use virtualenv python [skip ci].

--- .travis_run.sh
+++ .travis_run.sh
@@ -7,7 +7,7 @@ mkdir -p $TEMPDIR
 # directory.
 ( 
     cd $TEMPDIR
-    cmake -DPYTHON_EXECUTABLE=/usr/bin/python $REPODIR && make -j4 
+    cmake $REPODIR && make -j4 
 
     echo "We did not run following scripts"
     if [[ -f SUCCEEDED ]]; then
