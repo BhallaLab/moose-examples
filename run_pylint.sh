ce3d1017eb0b46fe436325c4410612e59cc9ba48
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-19 13:07:18 +0530

    Two more scripts are broken #30. Added script to run pylint.

--- run_pylint.sh
+++ run_pylint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -e
-PYLINT="pylint -E --disable=no-member --disable=no-name-in-module \
+PYLINT="python3 -m pylint -E --disable=no-member --disable=no-name-in-module \
     --disable=invalid-unary-operand-type"
 FILES=$(find . -type f -name "*.py" | shuf)
 for f in $FILES; do 
     echo "Checking $f"
-    $PYLINT $@ $f 
+    DIR=$(dirname $f)
+    SNAME=$(basename $f)
+    ( 
+        cd $DIR
+        $PYLINT $@ $SNAME
+    )
 done
