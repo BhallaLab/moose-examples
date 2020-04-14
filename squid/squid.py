d7ec818b70b2cf557986002eb04206571175d6ce
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-14 22:05:36 +0530

    temp commit [skip ci]

--- squid/squid.py
+++ squid/squid.py
@@ -1,34 +1,10 @@
-# squid.py ---
-#
 # Filename: squid.py
 # Description:
 # Author: Subhasis Ray
-# Maintainer:
-# Created: Mon Feb 13 11:35:11 2012 (+0530)
-# Version:
-# Last-Updated: Thu Sep 20 14:05:27 2012 (+0530)
-#           By: subha
-#     Update #: 771
-# URL:
-# Keywords:
-# Compatibility:
-#
-#
+# Maintainer: Dilawar Singh
 
-# Commentary:
-#
-# Rewrite of the squid demo for new moose.
-#
-#
-
-# Change log:
-#
-#
-#
-
-# Code:
 import sys
-import numpy
+import numpy as np
 import moose
 
 GAS_CONSTANT = 8.314
@@ -36,13 +12,17 @@ FARADAY = 9.65e4
 CELSIUS_TO_KELVIN = 273.15
 
 
-class IonChannel(moose.HHChannel):
+class IonChannel(object):
     """Enhanced version of HHChannel with setupAlpha that takes a dict
     of parameters."""
-
-    def __init__(
-        self, name, compartment, specific_gbar, e_rev, Xpower, Ypower=0.0, Zpower=0.0
-    ):
+    def __init__(self,
+                 name,
+                 compartment,
+                 specific_gbar,
+                 e_rev,
+                 Xpower,
+                 Ypower=0.0,
+                 Zpower=0.0):
         """Instantuate an ion channel.
 
         name -- name of the channel.
@@ -57,14 +37,14 @@ class IonChannel(moose.HHChannel):
 
         Ypower -- exponent for the second gatinmg component.
         """
-        moose.HHChannel.__init__(self, "%s/%s" % (compartment.path, name))
-        self.Gbar = specific_gbar * compartment.area
-        self.Ek = e_rev
-        self.Xpower = Xpower
-        self.Ypower = Ypower
-        self.Zpower = Zpower
-        moose.connect(self, "channel", compartment, "channel")
-        print(moose.showmsg(self))
+        self.path = "%s/%s" % (compartment.path, name)
+        self.chan = moose.HHChannel(self.path)
+        self.chan.Gbar = specific_gbar * compartment.area
+        self.chan.Ek = e_rev
+        self.chan.Xpower = Xpower
+        self.chan.Ypower = Ypower
+        self.chan.Zpower = Zpower
+        moose.connect(self.chan, "channel", compartment.compt, "channel")
 
     def setupAlpha(self, gate, params, vdivs, vmin, vmax):
         """Setup alpha and beta parameters of specified gate.
@@ -79,62 +59,57 @@ class IonChannel(moose.HHChannel):
 
         vmax -- maximum voltage value for the alpha/beta lookup tables.
         """
-        if gate == "X" and self.Xpower > 0:
+        if gate == "X" and self.chan.Xpower > 0:
             gate = moose.HHGate(self.path + "/gateX")
-        elif gate == "Y" and self.Ypower > 0:
+        elif gate == "Y" and self.chan.Ypower > 0:
             gate = moose.HHGate(self.path + "/gateY")
         else:
             return False
-        gate.setupAlpha(
-            [
-                params["A_A"],
-                params["A_B"],
-                params["A_C"],
-                params["A_D"],
-                params["A_F"],
-                params["B_A"],
-                params["B_B"],
-                params["B_C"],
-                params["B_D"],
-                params["B_F"],
-                vdivs,
-                vmin,
-                vmax,
-            ]
-        )
-        print("111", gate.alpha)
+        gate.setupAlpha([
+            params["A_A"],
+            params["A_B"],
+            params["A_C"],
+            params["A_D"],
+            params["A_F"],
+            params["B_A"],
+            params["B_B"],
+            params["B_C"],
+            params["B_D"],
+            params["B_F"],
+            vdivs,
+            vmin,
+            vmax,
+        ])
         return True
 
-    @property
     def alpha_m(self):
-        if self.Xpower == 0:
-            return numpy.array([])
-        return numpy.array(moose.element("%s/gateX" % (self.path)).tableA)
+        if self.chan.Xpower == 0:
+            return np.array([])
+        return np.array(moose.element("%s/gateX" % (self.path)).tableA)
 
-    @property
     def beta_m(self):
-        if self.Xpower == 0:
-            return numpy.array([])
-        return numpy.array(
-            moose.element("%s/gateX" % (self.path)).tableB
-        ) - numpy.array(moose.element("%s/gateX" % (self.path)).tableA)
+        if self.chan.Xpower == 0:
+            return np.array([])
+        return np.array(moose.element("%s/gateX" %
+                                      (self.path)).tableB) - np.array(
+                                          moose.element("%s/gateX" %
+                                                        (self.path)).tableA)
 
-    @property
     def alpha_h(self):
-        if self.Ypower == 0:
-            return numpy.array([])
-        return numpy.array(moose.element("%s/gateY" % (self.path)).tableA)
+        if self.chan.Ypower == 0:
+            return np.array([])
+        return np.array(moose.element("%s/gateY" % (self.path)).tableA)
 
-    @property
     def beta_h(self):
-        if self.Ypower == 0:
-            return numpy.array([])
-        return numpy.array(
-            moose.element("%s/gateY" % (self.path)).tableB
-        ) - numpy.array(moose.element("%s/gateY" % (self.path)).tableA)
+        if self.chan.Ypower == 0:
+            return np.array([])
+        return np.array(moose.element("%s/gateY" %
+                                      (self.path)).tableB) - np.array(
+                                          moose.element("%s/gateY" %
+                                                        (self.path)).tableA)
 
 
-class SquidAxon(moose.Compartment):
+class SquidAxon(object):
     EREST_ACT = 0.0  # can be -70 mV if not following original HH convention
     VMIN = -30.0
     VMAX = 120.0
@@ -144,7 +119,7 @@ class SquidAxon(moose.Compartment):
         "K_out": 10.0,
         "Na_out": 460.0,
         "K_in": 301.4,
-        "Na_in": 70.96,
+        "Na_in": 70.97,
         "Cl_out": 540.0,
         "Cl_in": 100.0,
         "length": 500.0,  # um
@@ -196,9 +171,9 @@ class SquidAxon(moose.Compartment):
     }
     """Compartment class enhanced with specific values of passive
     electrical properties set and calculated using dimensions."""
-
     def __init__(self, path):
-        moose.Compartment.__init__(self, path)
+        self.path = path
+        self.compt = moose.Compartment(self.path)
         self.temperature = SquidAxon.defaults["temperature"]
         self.K_out = SquidAxon.defaults["K_out"]
         self.Na_out = SquidAxon.defaults["Na_out"]
@@ -210,25 +185,27 @@ class SquidAxon(moose.Compartment):
         self.Cl_out = SquidAxon.defaults["Cl_out"]
         self.Cl_in = SquidAxon.defaults["Cl_in"]
 
-        self.length = SquidAxon.defaults["length"]
-        self.diameter = SquidAxon.defaults["diameter"]
-        self.Em = SquidAxon.defaults["Em"]
-        self.initVm = SquidAxon.defaults["initVm"]
+        self.compt.length = SquidAxon.defaults["length"]
+        self.compt.diameter = SquidAxon.defaults["diameter"]
+        self.compt.Em = SquidAxon.defaults["Em"]
+        self.compt.initVm = SquidAxon.defaults["initVm"]
         self.specific_cm = SquidAxon.defaults["specific_cm"]
         self.specific_gl = SquidAxon.defaults["specific_gl"]
         self.specific_ra = SquidAxon.defaults["specific_ra"]
 
-        self.Na_channel = IonChannel("Na", self, 0.0, self.VNa, Xpower=3.0, Ypower=1.0)
-        self.Na_channel.setupAlpha(
-            "X", SquidAxon.Na_m_params, SquidAxon.VDIVS, SquidAxon.VMIN, SquidAxon.VMAX
-        )
-        self.Na_channel.setupAlpha(
-            "Y", SquidAxon.Na_h_params, SquidAxon.VDIVS, SquidAxon.VMIN, SquidAxon.VMAX
-        )
+        self.Na_channel = IonChannel("Na",
+                                     self,
+                                     0.0,
+                                     self.VNa,
+                                     Xpower=3.0,
+                                     Ypower=1.0)
+        self.Na_channel.setupAlpha("X", SquidAxon.Na_m_params, SquidAxon.VDIVS,
+                                   SquidAxon.VMIN, SquidAxon.VMAX)
+        self.Na_channel.setupAlpha("Y", SquidAxon.Na_h_params, SquidAxon.VDIVS,
+                                   SquidAxon.VMIN, SquidAxon.VMAX)
         self.K_channel = IonChannel("K", self, 0.0, self.VK, Xpower=4.0)
-        self.K_channel.setupAlpha(
-            "X", SquidAxon.K_n_params, SquidAxon.VDIVS, SquidAxon.VMIN, SquidAxon.VMAX
-        )
+        self.K_channel.setupAlpha("X", SquidAxon.K_n_params, SquidAxon.VDIVS,
+                                  SquidAxon.VMIN, SquidAxon.VMAX)
         self.specific_gNa = SquidAxon.defaults["specific_gNa"]
         self.specific_gK = SquidAxon.defaults["specific_gK"]
 
@@ -236,54 +213,51 @@ class SquidAxon(moose.Compartment):
     def reversal_potential(cls, temp, c_out, c_in):
         """Compute the reversal potential based on Nernst equation."""
         # NOTE the 70 mV added for compatibility with original HH
-        v = (
-            (GAS_CONSTANT * temp / FARADAY) * 1000.0 * numpy.log(c_out / c_in)
-            + 70.0
-            + cls.EREST_ACT
-        )
+        v = ((GAS_CONSTANT * temp / FARADAY) * 1000.0 * np.log(c_out / c_in) +
+             70.0 + cls.EREST_ACT)
         return v
 
     @property
     def xarea(self):
         """Area of cross section in cm^2 when length and diameter are in um"""
-        return 1e-8 * numpy.pi * self.diameter * self.diameter / 4.0  # cm^2
+        return 1e-8 * np.pi * self.compt.diameter * self.compt.diameter / 4.0  # cm^2
 
     @property
     def area(self):
         """Area in cm^2 when length and diameter are in um"""
-        return 1e-8 * self.length * numpy.pi * self.diameter  # cm^2
+        return 1e-8 * self.compt.length * np.pi * self.compt.diameter  # cm^2
 
     @property
     def specific_ra(self):
-        return self.Ra * self.xarea / self.length
+        return self.compt.Ra * self.xarea / self.compt.length
 
     @specific_ra.setter
     def specific_ra(self, value):
-        self.Ra = value * self.length / self.xarea
+        self.compt.Ra = value * self.compt.length / self.xarea
 
     @property
     def specific_cm(self):
-        return self.Cm / self.area
+        return self.compt.Cm / self.area
 
     @specific_cm.setter
     def specific_cm(self, value):
-        self.Cm = value * self.area
+        self.compt.Cm = value * self.area
 
     @property
     def specific_gl(self):
-        return 1.0 / (self.Rm * self.area)
+        return 1.0 / (self.compt.Rm * self.area)
 
     @specific_gl.setter
     def specific_gl(self, value):
-        self.Rm = 1.0 / (value * self.area)
+        self.compt.Rm = 1.0 / (value * self.area)
 
     @property
     def specific_rm(self):
-        return self.Rm * self.area
+        return self.compt.Rm * self.area
 
     @specific_rm.setter
     def specific_rm(self, value):
-        self.Rm = value / self.area
+        self.compt.Rm = value / self.area
 
     @property
     def specific_gNa(self):
@@ -304,12 +278,14 @@ class SquidAxon(moose.Compartment):
     @property
     def VK(self):
         """Reversal potential of K+ channels"""
-        return SquidAxon.reversal_potential(self.temperature, self.K_out, self.K_in)
+        return SquidAxon.reversal_potential(self.temperature, self.K_out,
+                                            self.K_in)
 
     @property
     def VNa(self):
         """Reversal potential of Na+ channels"""
-        return SquidAxon.reversal_potential(self.temperature, self.Na_out, self.Na_in)
+        return SquidAxon.reversal_potential(self.temperature, self.Na_out,
+                                            self.Na_in)
 
     def updateEk(self):
         """Update the channels' Ek"""
@@ -329,37 +305,32 @@ class SquidAxon(moose.Compartment):
             setattr(self, field, value)
 
 
-class SquidModel(moose.Neutral):
+class SquidModel:
     """Container for squid demo."""
-
     def __init__(self, path):
-        moose.Neutral.__init__(self, path)
-        self.squid_axon = SquidAxon(path + "/squid_axon")
-        print((self.squid_axon.Na_channel.Gbar, self.squid_axon.K_channel.Gbar))
+        self.path = path
+        moose.Neutral(self.path)
+        self.squid = SquidAxon(path + "/squidAxon")
         self.current_clamp = moose.PulseGen(path + "/pulsegen")
         self.current_clamp.firstDelay = 5.0  # ms
         self.current_clamp.firstWidth = 40  # ms
         self.current_clamp.firstLevel = 0.1  # uA
         self.current_clamp.secondDelay = 1e9
-        print(
-            (
-                "Current clamp connected:",
-                moose.connect(
-                    self.current_clamp, "output", self.squid_axon, "injectMsg"
-                ),
-            )
-        )
+        moose.connect(self.current_clamp, "output", self.squid.compt,
+                      "injectMsg")
 
         self.Vm_table = moose.Table("%s/Vm" % (self.path))
-        moose.connect(self.Vm_table, "requestOut", self.squid_axon, "getVm")
+        moose.connect(self.Vm_table, "requestOut", self.squid.compt, "getVm")
         self.gK_table = moose.Table("%s/gK" % (self.path))
-        moose.connect(self.gK_table, "requestOut", self.squid_axon.K_channel, "getGk")
+        moose.connect(self.gK_table, "requestOut", self.squid.K_channel.chan,
+                      "getGk")
         self.gNa_table = moose.Table("%s/gNa" % (self.path))
-        moose.connect(self.gNa_table, "requestOut", self.squid_axon.Na_channel, "getGk")
+        moose.connect(self.gNa_table, "requestOut", self.squid.Na_channel.chan,
+                      "getGk")
         self.clocks_assigned = False
 
     def run(self, runtime, simdt=1e-6):
-        self.squid_axon.updateEk()
+        self.squid.updateEk()
         moose.reinit()
         moose.start(runtime)
 
@@ -368,19 +339,20 @@ class SquidModel(moose.Neutral):
         print("Vm saved to Vm.dat")
         if hasattr(self, "gK_table"):
             self.gK_table.xplot("gK.dat", "gK")
-            numpy.savetxt("K_alpha_n.dat", self.squid_axon.K_channel.alpha_m)
-            numpy.savetxt("K_beta_n.dat", self.squid_axon.K_channel.beta_m)
+            np.savetxt("K_alpha_n.dat", self.squid.K_channel.alpha_m)
+            np.savetxt("K_beta_n.dat", self.squid.K_channel.beta_m)
             print("K conductance saved to gK.dat")
         if hasattr(self, "gNa_table"):
             self.gNa_table.xplot("gNa.dat", "gNa")
-            numpy.savetxt("Na_alpha_m.dat", self.squid_axon.Na_channel.alpha_m)
-            numpy.savetxt("Na_beta_m.dat", self.squid_axon.Na_channel.beta_m)
-            numpy.savetxt("Na_alpha_h.dat", self.squid_axon.Na_channel.alpha_h)
-            numpy.savetxt("Na_beta_h.dat", self.squid_axon.Na_channel.beta_h)
+            np.savetxt("Na_alpha_m.dat", self.squid.Na_channel.alpha_m)
+            np.savetxt("Na_beta_m.dat", self.squid.Na_channel.beta_m)
+            np.savetxt("Na_alpha_h.dat", self.squid.Na_channel.alpha_h)
+            np.savetxt("Na_beta_h.dat", self.squid.Na_channel.beta_h)
             print("Na conductance saved to gNa.dat")
 
     def plot_data(self):
         import matplotlib.pyplot as plt
+
         ax11 = plt.subplot(221)
         ax12 = plt.subplot(222)
         ax21 = plt.subplot(223)
@@ -393,7 +365,6 @@ class SquidModel(moose.Neutral):
         plt.show()
 
 
-
 def test(runtime=100.0, simdt=1e-2):
     model = SquidModel("/model")
     model.run(runtime, simdt)
@@ -402,9 +373,4 @@ def test(runtime=100.0, simdt=1e-2):
 
 
 if __name__ == "__main__":
-    # unittest.main()
     test()
-
-
-#
-# squid.py ends here
