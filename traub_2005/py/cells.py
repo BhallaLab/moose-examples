36be3fe5e468c72962ad320c7049f53dbf50025b
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-15 17:52:27 +0530

    Fixed figure fig_a2 and fig_a3.

--- traub_2005/py/cells.py
+++ traub_2005/py/cells.py
@@ -198,15 +198,14 @@ class CellMeta(moose.melement.__class__):
 
     
 @metafix.with_metaclass(CellMeta)
-class CellBase(moose.Neuron):
-    # __metaclass__ = CellMeta  # This is incompatible with Python3
+class CellBase(object):
     annotation = {'cno': 'cno_0000020'}
     def __init__(self, path):
-        if not moose.exists(path):
+        self.path = path
+        if not moose.exists(self.path):
             path_tokens = path.rpartition('/')
             moose.copy(self.prototype, path_tokens[0], path_tokens[-1])
         
-        moose.element( path )
         self.solver = moose.HSolve('{}/solver'.format(path))
         self.solver.target = path
         self.solver.dt = config.simulationSettings.simulationDt
