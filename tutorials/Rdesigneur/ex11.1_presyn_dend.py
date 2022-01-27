ee5b6853cd64614852fb93372e91b374ff8a8cc3
HarshaRani <ranishashi@gmail.com>
2022-01-28 02:18:02 +0530

    some more addition examples added to Rdesigneur

--- tutorials/Rdesigneur/ex11.1_presyn_dend.py
+++ tutorials/Rdesigneur/ex11.1_presyn_dend.py
@@ -26,14 +26,14 @@ rdes = rd.rdesigneur(
     useGssa = True,    
 
     # cellProto syntax: ['ballAndStick', 'name', somaDia, somaLength, dendDia, dendLength, numDendSeg]
-    # 10x10 micron soma, 2x60 micron dendrite, 1 segment dendrite.
-    cellProto = [['ballAndStick', 'soma',  10e-6, 10e-6, 2e-6, 60e-6, 1]],
+    # 6x6 micron soma, 1.5x60 micron dendrite, 1 segment dendrite.
+    cellProto = [['ballAndStick', 'soma',  6e-6, 6e-6, 1.5e-6, 20e-6, 1]],
     chanProto = [['make_glu()', 'glu']],
     chanDistrib = [['glu', 'dend#', 'Gbar', '0.1']],
-    chemProto = [['./chem/echem.g', 'chem']],
+    chemProto = [['chem/echem.g', 'chem']],
     chemDistrib = [ # Put presynaptic bouton compartments next to the dend
         # Args: chem_model, elec_compts, mesh_type, spatial_distrib, r, sdev, spacing
-        ['kinetics', 'dend#', 'presyn_dend', '1', 0.26e-6, 0, 20e-6 ],
+        ['kinetics', 'dend#', 'presyn_dend', '1', 0.26e-6, 0, 5e-6 ],
     ],
     adaptorList = [ # map released neurotransmitter to cognate receptor
         # Want et al JACS 2019, 141,44 estimate 8K glu per vesicle.
@@ -48,11 +48,16 @@ rdes = rd.rdesigneur(
         ['#', '1', 'kinetics/glu/Ca_ext', 'conc', 'Input to bouton'],
         ['#', '1', 'kinetics/glu/glu', 'n', '# of glu vesicles released'],
         ['soma', '1', '.', 'Vm', 'Membrane potential'],
+    ],
+    moogList = [
+        ['#', '1', '.', 'Vm', 'Membrane potential', -65.0, -55.0],
+        ['#', '1', 'kinetics/glu/Ca', 'conc', 'Ca conc', 0.0, 1.0]
     ]
 )
 
 moose.seed( 1234 ) # Random number seed. Response details change with this.
 rdes.buildModel() # Assemble the model from prototypes.
 moose.reinit()
-moose.start( runtime )
-rdes.display()
+rdes.displayMoogli( 0.001, runtime, rotation = 0.00, mergeDisplays=True )
+#moose.start( runtime )
+#rdes.display()
